04bc2b9c317a43c209283473f03058c28fb6f954
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-11-08 10:18:37 -0500

    update JoystickController

--- Assets/Personal/Mathieu/Script/JoystickController.cs
+++ Assets/Personal/Mathieu/Script/JoystickController.cs
@@ -5,67 +5,52 @@ using UnityEngine.EventSystems;
 
 public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+    /*
+     * This script has to be in a canvas
+     */
     #region var
 
     [SerializeField] private RectTransform rtBack;
     [SerializeField] private RectTransform rtJoystick;
 
-    public Transform player;
     private float radius;
-    [SerializeField] private float playerSpeed;
 
-    private Vector3 VecMove;
-    bool touch = false;
+    public Vector3 vecJoystick;
 
     #endregion
 
-    // Start is called before the first frame update
     void Start()
     {
         radius = rtBack.rect.width / 2;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(touch)
-        {
-            player.position = VecMove;
-        }
     }
 
     void OnTouch(Vector2 vecTouch)
     {
         Vector2 vec = new Vector2 (vecTouch.x - rtBack.position.x, vecTouch.y - rtBack.position.y);
 
-        // 
+        //Clamp the joystick inside of the joystick background
         vec = Vector2.ClampMagnitude(vec, radius);
         rtJoystick.localPosition = vec;
 
-        float fSqr = (rtBack.position - rtJoystick.position).sqrMagnitude / radius * radius;
+        //Normalize vec
+        vec = new Vector2(vec.x / rtBack.rect.width, vec.y / rtBack.rect.height) * 2;
 
-        Vector2 vecNormal = vec.normalized;
-
-        VecMove = new Vector3(vecNormal.x * playerSpeed * Time.deltaTime * fSqr, 0f, vecNormal.y * playerSpeed * Time.deltaTime * fSqr);
-        player.eulerAngles = new Vector3(0f, Mathf.Atan2(vecNormal.x,vecNormal.y) * Mathf.Rad2Deg, 0f);
+        vecJoystick = new Vector3(vec.x,0f, vec.y);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         OnTouch(eventData.position);
-        touch = true;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         OnTouch(eventData.position);
-        touch = true;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         rtJoystick.localPosition = Vector3.zero;
-        touch = false;
+        vecJoystick = Vector3.zero;
     }
-
 }
