8572129f41814d5bada99171451c8a670e929e4c
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-11-20 15:00:26 -0500

    Mercy behaviour, updates baby script, add readme
    need to clean more code

--- Assets/Scripts/BabyBehaviour.cs
+++ Assets/Scripts/BabyBehaviour.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BabyBehaviour : MonoBehaviour
 {
+    [HideInInspector] public Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Player"))
@@ -11,7 +18,5 @@ public class BabyBehaviour : MonoBehaviour
             PlayerBabyInteraction playerBabyInteraction = col.gameObject.GetComponentInParent<PlayerBabyInteraction>();
             playerBabyInteraction.BabyTouch(this.gameObject);
         }
-            
-
     }
 }
