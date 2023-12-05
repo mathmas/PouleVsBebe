7e6980ee4313234367f7740924d15169b1629595
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-12-05 10:13:25 -0500

    update mercy path with transform position, add button for the menu

--- Assets/Scripts/MercyPathScriptableObject.cs
+++ Assets/Scripts/MercyPathScriptableObject.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MercyPath", menuName = "MercyPath")]
 public class MercyPathScriptableObject : ScriptableObject
 {
-    public List<Vector3> points = new List<Vector3>();
+    public List<Transform> points = new List<Transform>();
 }
