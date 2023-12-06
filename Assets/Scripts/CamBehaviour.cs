56cfffc9a64fa03ce124b6562f52eb36450eb84c
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-12-06 17:01:54 -0500

    update cam script

--- Assets/Scripts/CamBehaviour.cs
+++ Assets/Scripts/CamBehaviour.cs
@@ -6,21 +6,43 @@ public class CamBehaviour : MonoBehaviour
 {
     #region variables
 
+    [SerializeField] public bool isGameStarted;
+
     [Tooltip("GameObject Camera will follow")]
     [SerializeField] private Transform target;
 
     [Range(0f, 1f)]
     [SerializeField] private float smoothSpeed;
 
-    [Tooltip("Distance between the camera and the target")]
-    [SerializeField] private Vector3 offSet;
+    [Space(5f)]
+    [Tooltip("Distance between the camera and the target when the player is in the main menu")]
+    [SerializeField] private Vector3 startOffSet;
+
+    [Space(5f)]
+    [Tooltip("Distance between the camera and the target when the player is playing")]
+    [SerializeField] private Vector3 playingOffSet;
 
     #endregion
 
+    private void Start()
+    {
+        isGameStarted = false;
+    }
     private void FixedUpdate()
     {
+        Vector3 offSet;
+        if(!isGameStarted)
+        {
+            offSet = startOffSet;
+        }else
+        {
+            offSet = playingOffSet;
+        }
+
         Vector3 desiredPosition = target.position + offSet;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
+
+
 }
