dafa20251f5115e74fa45e20fce708abec3047a0
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-12-11 11:35:09 -0500

    baby fix

--- Assets/Scripts/PlayerBabyInteraction.cs
+++ Assets/Scripts/PlayerBabyInteraction.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerBabyInteraction : MonoBehaviour
@@ -14,9 +15,10 @@ public class PlayerBabyInteraction : MonoBehaviour
         GetComponent<PlayerMovement>().isDiscovered = true;
 
         //To be changed
-        baby.transform.position = transform.position + new Vector3(0f, 0.5f, 0.5f);
-        baby.transform.localScale = new Vector3 (0.8f, 0.8f, 0.8f);
         baby.transform.SetParent(transform, true);
+        baby.transform.position = transform.position + new Vector3(0f, 0.2f, 0f);
+        baby.transform.localScale = new Vector3 (0.8f, 0.8f, 0.8f);
+        baby.transform.localRotation = new quaternion(0, 0,0,0);
 
     }
 }
