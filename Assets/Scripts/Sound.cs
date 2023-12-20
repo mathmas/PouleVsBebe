237bdc3bc3da2b4d8770201526a5e39c8a5e318e
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-12-20 10:43:10 -0500

    add sound + script all button sounds

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public void PlayAudioSourceSound()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
